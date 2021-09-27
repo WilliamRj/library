58cfcc581df3ea129be664774c2c525c6b4d0b0a
William Pereira <William Pereira@host.docker.internal>
2021-09-27 17:26:07 -0300

    Criado classe cliente e livro

---
+++ library/src/main/java/br/edu/infnet/library/model/Cliente.java
@@ -0,0 +1,69 @@
+package br.edu.infnet.library.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente {
+	
+	@Id @GeneratedValue
+	private Integer codigo_cliente;
+	
+	private String nome;
+	private String endereco;
+	private String telefone;
+	private String email;
+	
+	public Cliente () {
+		
+	}
+
+	public Cliente(String nome, String endereco, String telefone, String email) {
+		super();
+		this.nome = nome;
+		this.endereco = endereco;
+		this.telefone = telefone;
+		this.email = email;
+	}
+
+	public Integer getCodigo() {
+		return codigo_cliente;
+	}
+
+	public void setCodigo(Integer codigo_cliente) {
+		this.codigo_cliente = codigo_cliente;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
---
+++ library/src/main/java/br/edu/infnet/library/model/Livro.java
@@ -0,0 +1,72 @@
+package br.edu.infnet.library.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Livro {
+	
+	@Id @GeneratedValue
+	private Integer codigo_livro;
+	
+	private String titulo_livro;
+	private String autor_livro;
+	private String categoria_livro;
+	private Date data_publicacao;
+	
+	public Livro() {
+		
+	}
+
+	public Livro(String titulo_livro, String autor_livro, String categoria_livro, Date data_publicacao) {
+		super();
+		this.titulo_livro = titulo_livro;
+		this.autor_livro = autor_livro;
+		this.categoria_livro = categoria_livro;
+		this.data_publicacao = data_publicacao;
+	}
+
+	public Integer getCodigo_livro() {
+		return codigo_livro;
+	}
+
+	public void setCodigo_livro(Integer codigo_livro) {
+		this.codigo_livro = codigo_livro;
+	}
+
+	public String getTitulo_livro() {
+		return titulo_livro;
+	}
+
+	public void setTitulo_livro(String titulo_livro) {
+		this.titulo_livro = titulo_livro;
+	}
+
+	public String getAutor_livro() {
+		return autor_livro;
+	}
+
+	public void setAutor_livro(String autor_livro) {
+		this.autor_livro = autor_livro;
+	}
+
+	public String getCategoria_livro() {
+		return categoria_livro;
+	}
+
+	public void setCategoria_livro(String categoria_livro) {
+		this.categoria_livro = categoria_livro;
+	}
+
+	public Date getData_publicacao() {
+		return data_publicacao;
+	}
+
+	public void setData_publicacao(Date data_publicacao) {
+		this.data_publicacao = data_publicacao;
+	}	
+
+}
